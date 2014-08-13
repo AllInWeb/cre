2a4bcc633ae50c2aaa7f9e914c860a1b209e8eb1
black <fabri4eg@gmail.com>
2014-08-13 16:43:57 +0300

    template

--- sites/all/themes/awards/retailer-register-page.tpl.php
+++ sites/all/themes/awards/retailer-register-page.tpl.php
@@ -1,7 +1,13 @@
-<?php echo drupal_render($form_register); ?>
-<br/>
-<br/>
-<br/>
-<br/>
-<?php //krumo($user); ?>
-<?php echo drupal_render($form_login); ?>
+<div class="login-container">
+
+    <div class="authorization">
+        <h3>Войти</h3>
+        <?php echo drupal_render($form_login); ?>
+    </div>
+
+    <div class="role-form">
+        <h3>Зарегистрировать сеть</h3>
+        <?php echo drupal_render($form_register); ?>
+    </div>
+
+</div>
