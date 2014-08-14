82cc362c21ddd03bea088053004414d3f813cd6f
intensity <intensity.web@gmail.com>
2014-08-14 11:06:05 +0300

    commit

--- sites/all/themes/awards/templates/node--_cre_retail_awards.tpl.php
+++ sites/all/themes/awards/templates/node--_cre_retail_awards.tpl.php
@@ -1,34 +1,6 @@
-<style>
-    table {
-        width: 800px;
-    }
-
-    tbody {
-        border: 0;
-    }
-
-    table td.right {
-        float: right;
-    }
-
-    table td {
-        float: left;
-
-    }
-
-    hr {
-        height: 10px;
-    }
-
-</style>
-<table>
-    <tr>
-        <td><?php print render($title); ?></td>
-        <td>
-            <hr/>
-        </td>
-        <td class="right"
-        <?php print render($content['field_votes']); ?></td>
-    </tr>
-</table>
+<div class="title-votes">
+    <?php print render($title); ?>
+</div>
+<div class="line"></div>
+<?php print render($content['field_votes']); ?>
 
--- sites/all/themes/awards/templates/views-view-unformatted--ustav-prime.tpl.php
+++ sites/all/themes/awards/templates/views-view-unformatted--ustav-prime.tpl.php
@@ -6,10 +6,8 @@
  *
  * @ingroup views_templates
  */
-
 ?>
 <?php if (!empty($title)): ?>
-
     <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
