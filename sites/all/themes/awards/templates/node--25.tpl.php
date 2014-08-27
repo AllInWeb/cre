b42a974837f5b5fb1a7e8b42eaa2f9daf0b605a0
intensity <intensity.web@gmail.com>
2014-08-27 17:54:41 +0300

    excel merge

--- sites/all/themes/awards/templates/node--25.tpl.php
+++ sites/all/themes/awards/templates/node--25.tpl.php
@@ -9,13 +9,5 @@ $user = user_load($user->uid);
 module_load_include('inc', 'user', 'user.pages');
 $form = drupal_get_form('user_profile_form', $user);
 echo drupal_render($form);
-$form['field_photo']['#access'] = false;
-$form['field_job']['#access'] = false;
-$form['field_company']['#access'] = false;
-$form['field_rireit']['#access'] = false;
-$form['field_fio_en']['#access'] = false;
-$form['field_field_job_en_']['#access'] = false;
-$form['field__company_en_']['#access'] = false;
-$form['field_riteil_en_']['#access'] = false;
- ?>
+?>
 <a class="voting-link" href="/voting/most_wanted_retailer">Перейти к следующему вопросу</a>
