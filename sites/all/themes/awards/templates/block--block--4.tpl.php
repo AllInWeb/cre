27e08a244c8438de682e22e6c61abf0db98ffd12
intensity <intensity.web@gmail.com>
2014-08-28 17:27:06 +0300

    done all corections

--- sites/all/themes/awards/templates/block--block--4.tpl.php
+++ sites/all/themes/awards/templates/block--block--4.tpl.php
@@ -1,12 +1,12 @@
 <?php
-
-//global $user;
-//if ($logged_in) {
-//    if ($user->roles[4] || $user->roles[5]) {
-//        echo 'Здравствуйте, ' . $user->field_fio['und'][0]['value'];
-//        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
-//    } else {
-//        echo 'Здравствуйте, ' . $user->name;
-//        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
-//    }
-//}
+global $user;
+$auth_user = user_load($user->uid);
+if ($logged_in) {
+    if (isset($auth_user->field_fio['und'][0]['value'])) {
+        echo 'Здравствуйте, ' . $auth_user->field_fio['und'][0]['value'];
+        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
+    } else {
+        echo 'Здравствуйте, ' . $user->name;
+        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
+    }
+}
