760451294ddff45953708e5444ad4ff95a6f630c
black <fabri4eg@gmail.com>
2014-08-28 16:40:03 +0300

    menu in content & exit link

---
+++ sites/all/themes/awards/templates/block--block--4.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+//global $user;
+//if ($logged_in) {
+//    if ($user->roles[4] || $user->roles[5]) {
+//        echo 'Здравствуйте, ' . $user->field_fio['und'][0]['value'];
+//        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
+//    } else {
+//        echo 'Здравствуйте, ' . $user->name;
+//        echo '<a href="/user/logout" class="exit-link">Выйти</a>';
+//    }
+//}
