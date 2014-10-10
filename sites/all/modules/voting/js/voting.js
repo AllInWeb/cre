1fc666d8923662b63f6370faee5eaf0f3c9bb200
intensity <intensity.web@gmail.com>
2014-10-10 12:36:42 +0300

    sec_vot

--- sites/all/modules/voting/js/voting.js
+++ sites/all/modules/voting/js/voting.js
@@ -1,8 +1,3 @@
 $(document).ready(function () {
-    console.log($('tr:nth-child(2) td input:radio'));
     $('tr:nth-child(2) td input:radio').attr('checked', true);
-//        .each(function () {
-//console.log($(this).first);
-//       $(this).attr('checked', true);
-//   });
 });
