89036fc71b78e6e1a53872545d636fa813762efe
intensity <intensity.web@gmail.com>
2014-10-10 17:28:57 +0300

    script

--- sites/all/modules/voting/js/voting.js
+++ sites/all/modules/voting/js/voting.js
@@ -1,3 +1,13 @@
-$(document).ready(function () {
-    $('tr:nth-child(2) td input:radio').attr('checked', true);
+$(function(){
+    $('#vote_submit_button').hide();
+    var answered = new Array();
+    $('input:radio').change(function(){
+        var $this= $(this);
+        if ($this.is(':checked')){
+            answered.push($this.attr('name'));
+        }
+        if (answered.length == $('table tr:nth-child(2) td').length) {
+            $('#vote_submit_button').show();
+        }
+    });
 });
