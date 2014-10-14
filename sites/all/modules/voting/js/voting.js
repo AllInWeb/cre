b3a1f038a85804bbab065ddf5291e03488c6b20a
black <fabri4eg@gmail.com>
2014-10-14 19:51:01 +0300

    Исправил говнокод!!!

--- sites/all/modules/voting/js/voting.js
+++ sites/all/modules/voting/js/voting.js
@@ -1,25 +1,19 @@
-var $dr = jQuery.noConflict();
-$dr(function(){
+var dr = jQuery.noConflict();
+dr(function(){
     $('#vote_submit_button').hide();
-    var answered = new Array();
-    $('input:radio').change(function(){
-        var $this= $(this);
-        if ($this.is(':checked')){
-            answered.push($this.attr('name'));
-        }
-        if (answered.length == $('table tr:nth-child(2) td').length) {
+    jQuery('input:radio').change(function(){
+        if (jQuery('.voting input:checked').length == 6) {
             $('#vote_submit_button').show();
         }
     });
 
-    $(document).on("keypress", 'form', function (e) {
-        var code = e.keyCode || e.which;
-        console.log(code);
-        if (code == 13) {
-            console.log('Inside');
-            e.preventDefault();
-            return false;
-        }
-    });
-
+//    jQuery(document).on("keypress", 'form', function (e) {
+//        var code = e.keyCode || e.which;
+//        console.log(code);
+//        if (code == 13) {
+//            console.log('Inside');
+//            e.preventDefault();
+//            return false;
+//        }
+//    });
 });
